4f58d71e37cd42b00a0482ce7da27f17f9daeb84
Psn <106792309+psnprasann@users.noreply.github.com>
2023-08-25 18:20:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/JayRaj8833/JS-link-bot.git
+git clone https://github.com/psnprasann/JS-link-bot.git
 cd /JS-link-bot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
